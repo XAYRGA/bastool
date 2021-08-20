b756f2259612dd5f2943ed1730eac1025c4b6955
XAYRGA <dzbush25@gmail.com>
2021-08-20 15:25:32 -0400

    Add project files.

---
+++ bastool/BinaryAudioSequence.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Be.IO;
+
+namespace bastool
+{
+    class BinaryAudioSequence
+    {
+#if !DEBUG
+        [JsonIgnore]
+#endif
+        public ushort EntryCount;
+        public byte Category;
+        public byte Unk1;
+#if !DEBUG
+        [JsonIgnore]
+#endif
+        public uint padding1;
+
+
+        public BASSoundEntry[] entries;
+
+        public void read(BeBinaryReader reader)
+        {
+            EntryCount = reader.ReadUInt16();
+            Category = reader.ReadByte();
+            Unk1 = reader.ReadByte();
+            padding1 = reader.ReadUInt32();
+
+            entries = new BASSoundEntry[EntryCount];
+            for (int e = 0; e < EntryCount; e++)
+                (entries[e] = new BASSoundEntry()).read(reader);
+        }
+
+        public void write(BeBinaryWriter writer)
+        {
+            writer.Write(EntryCount);
+            writer.Write(Category);
+            writer.Write(Unk1);
+            writer.Write(padding1);
+            for (int i = 0; i < entries.Length; i++)
+                entries[i].write(writer);
+        }
+
+    }
+    class BASSoundEntry
+    {
+        public uint SoundID;
+        public float FrameDelay;
+#if !DEBUG
+        [JsonIgnore]
+#endif
+        public uint padding1;
+        public float Pitch;
+#if !DEBUG
+        [JsonIgnore]
+#endif
+        public uint Mode;
+
+        public sbyte Panning;
+        public byte unk1;
+        public byte unk2;
+        public byte Velocity;
+
+#if !DEBUG
+        [JsonIgnore]
+#endif
+        public ulong padding3;
+
+        public void read(BeBinaryReader reader)
+        {
+            SoundID = reader.ReadUInt32();
+            FrameDelay = reader.ReadSingle();
+            padding1 = reader.ReadUInt32();
+            Pitch = reader.ReadSingle();
+            Mode = reader.ReadUInt32();
+            Panning = reader.ReadSByte();
+            unk1 = reader.ReadByte();
+            unk2 = reader.ReadByte();
+            Velocity = reader.ReadByte();
+            padding3 = reader.ReadUInt64();
+        }
+
+        public void write(BeBinaryWriter writer)
+        {
+            writer.Write(SoundID);
+            writer.Write(FrameDelay);
+            writer.Write(padding1);
+            writer.Write(Pitch);
+            writer.Write(Mode);
+            writer.Write(Panning);
+            writer.Write(unk1);
+            writer.Write(unk2);
+            writer.Write(Velocity);
+            writer.Write(padding3);
+        }
+    }
+}
---
+++ bastool/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using xayrga;
+using System.IO;
+using Be.IO;
+using Newtonsoft.Json;
+
+
+namespace bastool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.ForegroundColor = ConsoleColor.Red ;
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            Console.WriteLine("!BASTOOL build in debug mode, do not push into release!");
+            Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            args = ("unpack baby/bas/biribiri.bas").Split(" ");
+#endif
+            cmdarg.cmdargs = args;
+            var operation = cmdarg.assertArg(0, "Operation");
+
+            switch(operation)
+            {
+                case "pack":
+                    var packFile = cmdarg.assertArg(1, "Input File");
+                    var packOut = cmdarg.tryArg(2, null);
+                    if (packOut == null)
+                        packOut = $"{Path.GetFileNameWithoutExtension(packFile)}.bas";
+
+                    break;
+                case "unpack":
+                    var unpackFile = cmdarg.assertArg(1, "Input File");
+                    var unpackOut = cmdarg.tryArg(2, null);
+                    if (unpackOut == null)
+                        unpackOut = $"{Path.GetFileNameWithoutExtension(unpackFile)}.json";
+                    unpackBAS(unpackFile, unpackOut);                       
+                    break;
+                case "help":
+                default:
+                    cmdarg.assert(true, $"Unrecognized task {operation}");
+                    break;
+            }            
+        }
+
+        public static void unpackBAS(string file, string output)
+        {
+            cmdarg.assert(!File.Exists(file), $"File {file} doesn't exist or cannot be accessed.");
+            var stream = File.OpenRead(file);
+            var reader = new BeBinaryReader(stream);
+            var basData = new BinaryAudioSequence();
+            basData.read(reader); // Try to load BAS.
+            var jsondata = JsonConvert.SerializeObject(basData, Formatting.Indented);
+            File.WriteAllText(output, jsondata);
+            Console.WriteLine($"Success! Output {output}");
+        }
+
+        private static void packBAS(string file, string output)
+        {
+            cmdarg.assert(!File.Exists(file), $"File {file} doesn't exist or cannot be accessed.");
+            var outFile = File.OpenWrite(output);
+            var outWriter = new BeBinaryWriter(outFile);
+            var jData = File.ReadAllText(file);
+            BinaryAudioSequence bas = null;
+            try
+            {
+                bas = JsonConvert.DeserializeObject<BinaryAudioSequence>(jData);
+            } catch (Exception E)
+            {
+                cmdarg.assert($"JSON Parse error: {E.Message}");
+#if DEBUG 
+                Console.WriteLine(E.ToString());
+#endif
+                Environment.Exit(0);
+            }
+
+            bas.EntryCount = (ushort)bas.entries.Length; // update before write.
+            bas.write(outWriter);
+            outFile.Flush();
+            outFile.Close();
+            Console.WriteLine($"Success! Output {output}");
+        }
+    }
+}
